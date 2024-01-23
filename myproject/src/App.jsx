2d32691ccd13255e0fe25cba3031516211f7a0cd
Clement Beaurain <clementbeaurainpro@gmail.com>
2024-01-23 14:56:14 +0100

    after entretien

---
+++ myproject/src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [backendResponse, setBackendResponse] = useState('')
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:3000/')
+      console.log('response', response)
+      const data = await response.text()
+      console.log('data', data)
+      setBackendResponse(data)
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button>
+          My response is {backendResponse}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
